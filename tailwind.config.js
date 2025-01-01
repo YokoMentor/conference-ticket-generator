a429df2911cad76aeb244343a9250b6d521d41b6
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2025-01-01 23:20:58 +0200

    Installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "1234px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
