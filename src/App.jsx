018a6f95523a767a692e96e60c6b153e573881fb
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2025-01-04 20:32:06 +0200

    First container's layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
     <div className='flex flex-col min-h-screen justify-center items-center  bg-bg-mobile bg-no-repeat text-[60px] text-red-500'>
-      <div className='flex flex-col justify-center items-center text-center'>
-        <img className="w-[166px] h-[24px]" src={logoFull} alt="logo"></img>
-        <h1 className='text-heading text-[28px] font-bold leading-[2rem]'>Your Journey to Coding Conf 2025 Starts Here!</h1>
+      <div className='flex flex-col justify-center items-center text-center w-[344px]'>
+        <img className="w-[166px] h-[24px] mb-10" src={logoFull} alt="logo"></img>
+        <h1 className='text-heading text-[28px] font-bold leading-[2rem] mb-6'>Your Journey to Coding Conf 2025 Starts Here!</h1>
         <p className='text-paragraph text-[19px] leading-[1.5rem]'>Secure your spot at next year's biggest coding conference.</p>
       </div>
     </div>
