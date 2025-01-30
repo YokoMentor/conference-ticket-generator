bdc8006e690230102f25d8215987104db0100073
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2025-01-30 23:44:33 +0200

    Functionality for Generate Ticket Btn

---
+++ src/CongratsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+function CongratsPage(){
+    return (
+        <div className='flex flex-col min-h-screen justify-center items-center bg-bg-mobile md:bg-bg-desktop bg-no-repeat text-[60px] text-error'>Hello World!</div>
+    )
+}
+
+export default CongratsPage
