34d5c8f4ac1975f216904f7522aa4a4547bda447
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2025-02-22 21:54:26 +0200

    Added click to upload functionality

--- src/App.jsx
+++ src/App.jsx
@@ -113,13 +113,16 @@ function App() {
           </div>
           <div className='flex flex-col justify-center w-[344px] md:w-[460px]'>
             <h2 className='text-heading text-[19px] text-left mb-2.5'>Upload Avatar</h2>
-            <div className='flex flex-col justify-center items-center rounded-lg bg-bg-box bg-opacity-30 w-full h-[126px] border-[1px] border-border-box border-dashed mb-3 cursor-pointer'>
-              <div className='flex justify-center items-center rounded-lg bg-bg-box w-[50px] h-[50px] border-border-box bg-opacity-25 border-[1px] mb-4 mt-1'>
-                <img src={iconUpload} alt="upload" className='w-[29px] h-[29px]'/>
+            <div className='relative w-full h-[126px]'>
+              <input type="file" id="myfile" name="myfile" className='absolute w-full h-full opacity-0'></input>
+              <div className='flex flex-col justify-center items-center rounded-lg bg-bg-box bg-opacity-30 w-full h-[126px] border-[1px] border-border-box border-dashed mb-3 cursor-pointer'>
+                <div className='flex justify-center items-center rounded-lg bg-bg-box w-[50px] h-[50px] border-border-box bg-opacity-25 border-[1px] mb-4 mt-1'>
+                  <img src={iconUpload} alt="upload" className='w-[29px] h-[29px]'/>
+                </div>
+                <p className='text-paragraph text-[18px]'>Drag and drop or click to upload</p>
               </div>
-              <p className='text-paragraph text-[18px]'>Drag and drop or click to upload</p>
             </div>
-            <div className='flex'>
+            <div className='flex mt-3'>
               <img src={iconInfo} alt="info" className='w-[17px] h-[17px] mr-2'/>
               <p className='text-paragraph text-[12px]'>Upload your photo &#40;JPG or PNG, max size: 500KB&#41;.</p>
             </div>
