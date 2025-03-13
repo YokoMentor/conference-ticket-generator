46e79ca361ab5596bb8483d8f4afddd8ac2c8655
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2025-03-13 22:30:32 +0200

    Added avatar size error text

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   const [usernameError, setUsernameError] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [avatar, setAvatar] = useState(0);
+  const [avatarError, setAvatarError] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -106,7 +107,7 @@ function App() {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
     if (file.size > 500){
-      console.log("testing error");
+      setAvatarError(true);
     }
     const reader = new FileReader();
     reader.onload = function(e) {
@@ -147,6 +148,14 @@ function App() {
               <img src={iconInfo} alt="info" className='w-[17px] h-[17px] mr-2'/>
               <p className='text-paragraph text-[12px]'>Upload your photo &#40;JPG or PNG, max size: 500KB&#41;.</p>
             </div>
+            {avatarError && 
+                <div className='flex items-center text-error text-[12px] mt-3'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
+                  <path stroke="#e16151" stroke-linecap="round" stroke-linejoin="round" d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"/>
+                  <path fill="#e16151" d="M8.004 10.462V7.596ZM8 5.57v-.042Z"/>
+                  <path stroke="#e16151" stroke-linecap="round" stroke-linejoin="round" d="M8.004 10.462V7.596M8 5.569v-.042"/></svg>
+                  <p className='ml-2'>File too large. Please upload a photo under 500KB.</p>
+                </div>}
           </div>
           <form className='mt-5' onSubmit={handleSubmit}>
             <div className='flex flex-col w-[344px] md:w-[460px]'>
